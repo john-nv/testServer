a279b4ba3614a74ce45c772cbf6fff152a30005b
quang <quangngo1500@gmail.com>
2023-04-04 13:12:52 +0700

    update index

--- src/index.js
+++ src/index.js
@@ -2,9 +2,28 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = process.env.PORTSERVER
+const crypto = require('crypto')
+const jwt = require('jsonwebtoken')
+
+const {privateKey, publicKey} = crypto.generateKeyPairSync('rsa', {
+  modulusLength: 4096
+})
+
+console.log(privateKey)
+
+const token = jwt.sign({userId: 1234, roles: ['admin']}, privateKey, {
+  algorithm: 'RS256',
+  expiresIn: '1 days'
+})
+console.log(token)
+
+jwt.verify(token, publicKey, (err, decode)=>{
+  console.log('decode', decode)
+})
 
 app.get('/', (req, res) => {
-  res.send('test server')
+  res.send(publicKey)
+  
 })
 
 app.listen(port, () => {
